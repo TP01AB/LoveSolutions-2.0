1cd61b82a5d19c111adfb24572aaad0a3236125f
isra9shadow <isra9movil@hotmail.com>
2021-02-20 01:24:49 +0100

    Creaccion de proyecto

---
+++ citas/src/java/Auxiliar/Constantes.java
@@ -0,0 +1,15 @@
+package Auxiliar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author isra9
+ */
+public class Constantes {
+    
+}
